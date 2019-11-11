425700ae1f06b4f18750647847a21a9c8e223963
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:59:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,5 +4,24 @@ class School
     @name = name
     @roster = roster
   end
-
+  def add_student(name, grade)
+    if self.roster[grade]
+      self.roster[grade] << name
+    else 
+      self.roster[grade] = []
+      self.roster[grade] << name 
+    end
+  end 
+  def grade(grade)
+    self.roster[grade]
+  end
+  def sort
+    sorted = {}
+    self.roster.each do |grade, students|
+      sorted_students = students.sort 
+      sorted[grade] = students.sort 
+    end
+    sorted
+  end
+end
 # code here!
